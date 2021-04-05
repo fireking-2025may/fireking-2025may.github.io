feb1363191f6e9c591a6ace44ac3bcf8e53985d8
fireking-design <64699690+fireking-design@users.noreply.github.com>
2021-04-05 20:14:11 +0100

    Update neural-node.js
    Create Network class
    Create Self-Invoking function to train neural-network
    - Get's data
    - Creates Meta-data
    - Initiates Network
    - Trains and presents final accuracy

--- neural-node.js
+++ neural-node.js
@@ -1,9 +1,8 @@
 const { getMnistData } = require('./mnist-data-generation');
 const { writeFileSync } = require('fs');
-
 const math = require('mathjs');
 
-
+const Network = layers => {
 
     const shuffle = array => {
         let m = array.length, t, i;
@@ -43,7 +42,20 @@ const math = require('mathjs');
 
     return {
         train, 
+        predict
     }
 }
 
+(async () => {
+    const data = await getMnistData();
+    const epochs = 60;
+    const miniBatchSize = 10;
+    const learningRate = 0.1;
+    const lambda = 5;
+    const writeFrequency = 3000;
+    const save = true;
+    const showProgress = true
     const network = Network([28 * 28, 100, 10]);
+    network.train(data, { epochs, miniBatchSize, learningRate, lambda }, { writeFrequency, save, showProgress });
+    console.log('Final Accuracy: ', network.predict(data.testingData));
+})()
