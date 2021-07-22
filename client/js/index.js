6a2718afd143ea266815c5c9c85daf684ed90ee0
fireking-design <64699690+fireking-design@users.noreply.github.com>
2021-07-22 11:47:47 +0100

    Update index.js
    Comment out most of the un-needed neural network code and see if it still works

--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,4 @@
-const initCanvas = network => {
+const initCanvas = ({ predict }) => {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext("2d");
 
@@ -25,8 +25,9 @@ const initCanvas = network => {
             const inputIndex = Math.floor((pixelIndex - 3) / (4));
             inputs[inputIndex] += pixels.data[pixelIndex];
         }
-        document.getElementById('prediction').innerText = network.predict(inputs);
+        document.getElementById('prediction').innerText = predict(inputs);
     });
+
     canvas.addEventListener('mouseout', () => isDrawing = false);
 
     canvas.addEventListener('mousemove', e => {
@@ -38,35 +39,17 @@ const initCanvas = network => {
         context.closePath();
         [lastX, lastY] = [e.offsetX * width / 200, e.offsetY * height / 200];
     })
+
+    
 };
 
-const fetchNetwork = async () => (await (await fetch('/client/data/weights.json')).json());
-
-const Network = (suppliedNetwork, layers) => {
-
-    const network = suppliedNetwork || layers.slice(1).map((layer, layerIndex) => { // layers
-        return [...Array(layer)].map(() => { // neuron
-            return {
-                weights: [...Array(layers[layerIndex])].map(() => Math.random() * (1 - -1) - 1), // weights
-                newWeights: [...Array(layers[layerIndex])].map(() => 0), // newWeights
-                weightConstants: [...Array(layers[layerIndex])].map(() => ({ derivative: 0, cost: 0 })),
-                bias: Math.random() * (1 - -1) - 1,
-                newBias: 0,
-                output: 0, 
-                delta: 1
-            }
-        })
-    })
-
-    const sigmoid = value => 1 / (Math.exp(-value) + 1);
-
-    const sigmoidDerivative = output => output * (1 - output);
+const Network2 = (coefficients) => {
 
     const feedForward = inputs => {
         let activations = inputs;
-        const networkSize = network.length;
+        const networkSize = coefficients.length;
         for (let layerIndex = 0; layerIndex < networkSize; ++layerIndex) { // layers
-            const layer = network[layerIndex];
+            const layer = coefficients[layerIndex];
             const layerSize = layer.length;
             const newInputs = []
             for (let neuronIndex = 0; neuronIndex < layerSize; ++neuronIndex) { // neurons
@@ -84,98 +67,165 @@ const Network = (suppliedNetwork, layers) => {
         return activations;
     }
 
-    const backPropagation = (inputs, expectedOutput, learningRate) => {
-        for (let layerIndex = network.length - 1; layerIndex >= 0; --layerIndex) {
-            const layer = network[layerIndex];
-            if (layerIndex !== network.length - 1) {
-                for (let currentNeuronIndex = 0; currentNeuronIndex < layer.length; ++currentNeuronIndex) {
-                    const neuron = layer[currentNeuronIndex];
-                    let cost = 0;
-                    for (let nextNeuronIndex = 0; nextNeuronIndex < network[layerIndex + 1].length; ++nextNeuronIndex) {
-                        const nextNeuron = network[layerIndex + 1][nextNeuronIndex];
-                        cost += nextNeuron.weightConstants[currentNeuronIndex].derivative * nextNeuron.weightConstants[currentNeuronIndex].cost * nextNeuron.weights[currentNeuronIndex]
-                    }
-                    const derivative = sigmoidDerivative(layer[currentNeuronIndex].output);
-                    for (let weightIndex = 0; weightIndex < neuron.weights.length; ++weightIndex) {
-                        let previousOutput;
-                        if (layerIndex - 1 < 0) previousOutput = inputs[weightIndex]
-                        else previousOutput = network[layerIndex - 1][weightIndex].output;
-                        neuron.newWeights[weightIndex] -= (learningRate * previousOutput * derivative * cost);
-                        neuron.weightConstants[weightIndex].derivative = derivative;
-                        neuron.weightConstants[weightIndex].cost = cost;
-                    }
-                    neuron.newBias -= (learningRate * derivative * cost);
-                }
-            } else {
-                for (let currentNeuronIndex = 0; currentNeuronIndex < layer.length; ++currentNeuronIndex) {
-                    const neuron = layer[currentNeuronIndex];
-                    const derivative = sigmoidDerivative(neuron.output)
-                    const cost = neuron.output - expectedOutput[currentNeuronIndex];
-                    for (let weightIndex = 0; weightIndex < neuron.weights.length; ++weightIndex) {
-                        const previousOutput = network[layerIndex - 1][weightIndex].output;
-                        neuron.newWeights[weightIndex] -= (learningRate * previousOutput * derivative * cost);
-                        neuron.weightConstants[weightIndex].derivative = derivative;
-                        neuron.weightConstants[weightIndex].cost = cost;
-                    }
-                    neuron.newBias -= (learningRate * derivative * cost)
-                }
-            }
-        }
-    }
-
-    const updateWeights = () => {
-        for (let layerIndex = 0; layerIndex < network.length; ++layerIndex) { // layers
-            for (let neuronIndex = 0; neuronIndex < network[layerIndex].length; ++neuronIndex) {
-                const neuron = network[layerIndex][neuronIndex];
-                for (let inputIndex = 0; inputIndex < neuron.weights.length; ++inputIndex) {
-                    neuron.weights[inputIndex] = neuron.newWeights[inputIndex]
-                }
-                neuron.bias = neuron.newBias
-            }
-        }
-    }
-
-    const predictArray = data => {
-        return data.map(data => {
-            const output = feedForward(data.input);
-            const actual = output.indexOf(Math.max(...output)) + 1;
-            const ideal = data.output.indexOf(1) + 1;
-            return {
-                actual,
-                ideal
-            }
-        }).reduce((acc, data) => data.actual === data.ideal ? acc +  1 : acc, 0) / data.length
-    }
-
     const predict = inputs => {
         const outputs = feedForward(inputs);
         console.log(outputs);
         return outputs.indexOf(Math.max(...outputs));
     }
 
-    const train = (data, epochs, batches, learningRate) => {
-        for (let i = epochs; i > 0; --i) { // epoch
-            for (let j = 0; j < batches + 2; ++j) { // batch
-                const batch = data.splice(Math.floor(Math.random() * (data.length - (data.length / batches))), (data.length / batches));
-                for (let dataIndex = 0; dataIndex < batch.length; ++dataIndex) {
-                    const data = batch[dataIndex];
-                    feedForward(data.input);
-                    backPropagation(data.input, data.output, learningRate);
-                    updateWeights(data.input);
-                }
-            }
-        }
-    }
+    // const feedForward2 = input => {
+    //     const networkSize = layers.length - 1;
+    //     for (let layerIndex = 0; layerIndex < networkSize; ++layerIndex) { // layers
+    //         input = sigmoid(math.add(math.multiply(weights[layerIndex], input), biases[layerIndex]));
+    //     }
+    //     return input;
+    // }
 
     return {
-        predict,
-        predictArray,
-        feedForward,
-        train
+        feedForward, 
+        predict
     }
 }
+
+(async () => (await (await fetch('/client/data/weights.json')).json()))().then(coefficients => initCanvas(Network2(coefficients)))
+
+// const fetchNetwork = async () => (await (await fetch('/client/data/weights.json')).json());
+
+// fetchNetwork().then(network => {
+//     network = Network2(network);
+//     initCanvas(network);
+// })
+
+// const Network = (suppliedNetwork, layers) => {
+
+//     const network = suppliedNetwork || layers.slice(1).map((layer, layerIndex) => { // layers
+//         return [...Array(layer)].map(() => { // neuron
+//             return {
+//                 weights: [...Array(layers[layerIndex])].map(() => Math.random() * (1 - -1) - 1), // weights
+//                 newWeights: [...Array(layers[layerIndex])].map(() => 0), // newWeights
+//                 weightConstants: [...Array(layers[layerIndex])].map(() => ({ derivative: 0, cost: 0 })),
+//                 bias: Math.random() * (1 - -1) - 1,
+//                 newBias: 0,
+//                 output: 0, 
+//                 delta: 1
+//             }
+//         })
+//     })
+
+//     const sigmoid = value => 1 / (Math.exp(-value) + 1);
+
+//     const sigmoidDerivative = output => output * (1 - output);
+
+//     const feedForward = inputs => {
+//         let activations = inputs;
+//         const networkSize = network.length;
+//         for (let layerIndex = 0; layerIndex < networkSize; ++layerIndex) { // layers
+//             const layer = network[layerIndex];
+//             const layerSize = layer.length;
+//             const newInputs = []
+//             for (let neuronIndex = 0; neuronIndex < layerSize; ++neuronIndex) { // neurons
+//                 const neuron = layer[neuronIndex];
+//                 let neuronTotal = neuron.bias;
+//                 for (let weightIndex = 0; weightIndex < neuron.weights.length; ++weightIndex) { // weights
+//                     neuronTotal += neuron.weights[weightIndex] * activations[weightIndex];
+//                 }
+//                 const activation = sigmoid(neuronTotal);
+//                 neuron.output = activation;
+//                 newInputs.push(activation);
+//             }
+//             activations = newInputs;
+//         }
+//         return activations;
+//     }
+
+//     const backPropagation = (inputs, expectedOutput, learningRate) => {
+//         for (let layerIndex = network.length - 1; layerIndex >= 0; --layerIndex) {
+//             const layer = network[layerIndex];
+//             if (layerIndex !== network.length - 1) {
+//                 for (let currentNeuronIndex = 0; currentNeuronIndex < layer.length; ++currentNeuronIndex) {
+//                     const neuron = layer[currentNeuronIndex];
+//                     let cost = 0;
+//                     for (let nextNeuronIndex = 0; nextNeuronIndex < network[layerIndex + 1].length; ++nextNeuronIndex) {
+//                         const nextNeuron = network[layerIndex + 1][nextNeuronIndex];
+//                         cost += nextNeuron.weightConstants[currentNeuronIndex].derivative * nextNeuron.weightConstants[currentNeuronIndex].cost * nextNeuron.weights[currentNeuronIndex]
+//                     }
+//                     const derivative = sigmoidDerivative(layer[currentNeuronIndex].output);
+//                     for (let weightIndex = 0; weightIndex < neuron.weights.length; ++weightIndex) {
+//                         let previousOutput;
+//                         if (layerIndex - 1 < 0) previousOutput = inputs[weightIndex]
+//                         else previousOutput = network[layerIndex - 1][weightIndex].output;
+//                         neuron.newWeights[weightIndex] -= (learningRate * previousOutput * derivative * cost);
+//                         neuron.weightConstants[weightIndex].derivative = derivative;
+//                         neuron.weightConstants[weightIndex].cost = cost;
+//                     }
+//                     neuron.newBias -= (learningRate * derivative * cost);
+//                 }
+//             } else {
+//                 for (let currentNeuronIndex = 0; currentNeuronIndex < layer.length; ++currentNeuronIndex) {
+//                     const neuron = layer[currentNeuronIndex];
+//                     const derivative = sigmoidDerivative(neuron.output)
+//                     const cost = neuron.output - expectedOutput[currentNeuronIndex];
+//                     for (let weightIndex = 0; weightIndex < neuron.weights.length; ++weightIndex) {
+//                         const previousOutput = network[layerIndex - 1][weightIndex].output;
+//                         neuron.newWeights[weightIndex] -= (learningRate * previousOutput * derivative * cost);
+//                         neuron.weightConstants[weightIndex].derivative = derivative;
+//                         neuron.weightConstants[weightIndex].cost = cost;
+//                     }
+//                     neuron.newBias -= (learningRate * derivative * cost)
+//                 }
+//             }
+//         }
+//     }
+
+//     const updateWeights = () => {
+//         for (let layerIndex = 0; layerIndex < network.length; ++layerIndex) { // layers
+//             for (let neuronIndex = 0; neuronIndex < network[layerIndex].length; ++neuronIndex) {
+//                 const neuron = network[layerIndex][neuronIndex];
+//                 for (let inputIndex = 0; inputIndex < neuron.weights.length; ++inputIndex) {
+//                     neuron.weights[inputIndex] = neuron.newWeights[inputIndex]
+//                 }
+//                 neuron.bias = neuron.newBias
+//             }
+//         }
+//     }
+
+//     const predictArray = data => {
+//         return data.map(data => {
+//             const output = feedForward(data.input);
+//             const actual = output.indexOf(Math.max(...output)) + 1;
+//             const ideal = data.output.indexOf(1) + 1;
+//             return {
+//                 actual,
+//                 ideal
+//             }
+//         }).reduce((acc, data) => data.actual === data.ideal ? acc +  1 : acc, 0) / data.length
+//     }
+
+    // const predict = inputs => {
+    //     const outputs = feedForward(inputs);
+    //     console.log(outputs);
+    //     return outputs.indexOf(Math.max(...outputs));
+    // }
+
+//     const train = (data, epochs, batches, learningRate) => {
+//         for (let i = epochs; i > 0; --i) { // epoch
+//             for (let j = 0; j < batches + 2; ++j) { // batch
+//                 const batch = data.splice(Math.floor(Math.random() * (data.length - (data.length / batches))), (data.length / batches));
+//                 for (let dataIndex = 0; dataIndex < batch.length; ++dataIndex) {
+//                     const data = batch[dataIndex];
+//                     feedForward(data.input);
+//                     backPropagation(data.input, data.output, learningRate);
+//                     updateWeights(data.input);
+//                 }
+//             }
+//         }
+//     }
+
+//     return {
+//         predict,
+//         predictArray,
+//         feedForward,
+//         train
+//     }
+// }
     
-fetchNetwork().then(network => {
-    network = Network(network, [28 * 28, 14 * 14, 7 * 7, 10]);
-    initCanvas(network);
-})
